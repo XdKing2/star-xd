0a13560147959698e7170cd954547fe40541c4d3
AbdulRehman19721986 <ranaabdulrehman1986@gmail.com>
2025-12-01 14:07:45 +0500

    Update setting.js

--- setting.js
+++ setting.js
@@ -4,7 +4,7 @@ module.exports = {
   BOT_NAME: 'sᴛᴀʀ xᴅ', // ur bot name
   OWNER_NAME: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //your name
   OWNER_NUMBER: '263776388689', // add your phone number
-  SESSION_ID: '', // use sessin id if inbuilt pairing not working
+  SESSION_ID: 'IK~YUcwlZzR#c1oMkMDTykjHPeOfaGN_aUeqn5olxZujrbfM9AXktAc', // use sessin id if inbuilt pairing not working
   MODE: "public", // public or private
   TIMEZONE: 'Africa/Harare', // ur timezone
 };
