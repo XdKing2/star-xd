0432e9b07820793ace816cfee5d605506267e92b
Malvin King <malvintech2@gmail.com>
2026-02-10 23:08:43 +0200

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,10 +1,13 @@
-//=====💫 sᴛᴀʀ xᴅ🔥====
+//=====💫 sᴛᴀʀ  xᴅ🔥====
 
 module.exports = {
   BOT_NAME: 'sᴛᴀʀ xᴅ', // ur bot name
   OWNER_NAME: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //your name
-  OWNER_NUMBER: '263776388689', // add your phone number
-  SESSION_ID: '', // use sessin id if inbuilt pairing not working
-  MODE: "public", // public or private
-  TIMEZONE: 'Africa/Harare', // ur timezone
+  OWNER_NUMBER: '263714757857', // add your phone number
+  PREFIXES: ['.', '!', '#'], // Multiple prefix support you can add one or more
+  
+  
+  SESSION_ID: '', // add your session ID
+  PAIRING_NUMBER: '', // In Case if you don't have session id get pairing code by putting your number here
+  TIMEZONE: 'Africa/Harare' // ur timezone
 };
