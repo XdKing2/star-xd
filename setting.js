07536aa0b72fe1688603e3d584a1817fbfb27809
kouakouyvesrolandyao-del <kouakouyvesrolandyao@gmail.com>
2026-02-26 11:21:59 +0000

    Update setting.js

--- setting.js
+++ setting.js
@@ -7,7 +7,19 @@ module.exports = {
   PREFIXES: ['.', '!', '#'], // Multiple prefix support you can add one or more
   
   
+  SESSION_ID: '',//=====💫 sᴛᴀʀ  xᴅ🔥====
+
+module.exports = {
+  BOT_NAME: 'sᴛᴀʀ xᴅ', // ur bot name
+  OWNER_NAME: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //your name
+  OWNER_NUMBER: '263714757857', // add your phone number
+  PREFIXES: ['.', '!', '#'], // Multiple prefix support you can add one or more
+  
+  
   SESSION_ID: '', // add your session ID
+  PAIRING_NUMBER: '', // In Case if you don't have session id get pairing code by putting your number here
+  TIMEZONE: 'Africa/Harare' // ur timezone
+}; // add your session ID
   PAIRING_NUMBER: '', // In Case if you don't have session id get pairing code by putting your number here
   TIMEZONE: 'Africa/Harare' // ur timezone
 };
